08f3fd08e2616eec9c4b3740bfd68867afd1122a
Pinho13 <rafaelpinhotsilva@gmail.com>
2024-07-07 18:31:10 +0100

    Bug fixes

--- resources/upgradestorage/upgrades.gd
+++ resources/upgradestorage/upgrades.gd
@@ -7,6 +7,6 @@ class_name UpgradeStorage extends Resource
 @export_range(0,2,0.01,"or_greater","suffix:x") var bomb_gravity : float = 1
 @export_range(0,2,0.01,"or_greater","suffix:x") var bomb_speed : float = 1
 @export_range(0,2,0.01,"or_greater","suffix:x") var bomb_damage: float = 1
-@export_range(-90,90,0.01,"or_less","or_greater","suffix:°+") var bomb_spread: int = 0
+@export_range(-90,90,0.01,"or_less","or_greater","suffix:°+") var bomb_spread: int = 45
 @export_range(0,2,0.01,"or_greater","suffix:x") var explosion_radius :float = 1
 @export_range(0,20,0.01,"or_greater","suffix:+") var bomb_count :int = 0
