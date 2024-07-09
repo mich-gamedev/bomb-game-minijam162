1299860ed90483971f7813f824260b1ee5686c66
Pinho13 <rafaelpinhotsilva@gmail.com>
2024-07-09 23:18:57 +0100

    Fixes

--- components/health/EnemyCorpse.gd
+++ components/health/EnemyCorpse.gd
@@ -5,6 +5,8 @@ var velocity: Vector2
 var gravity: Vector2 = Vector2(0, 5)
 var start_pos: Vector2
 var maxHeight: bool = false
+var timer: float = 0
+var time_to_destroy: int = 5
 
 func _ready():
 	start_pos = global_position
@@ -14,3 +16,6 @@ func _ready():
 func _process(delta):
 	velocity += gravity * delta
 	global_position += velocity
+	timer += delta
+	if timer >= time_to_destroy:
+		queue_free()
